e3c3155736a5f7a3c1a33b092683f9be8ae9f72f
Neukluziy <neukluziy@gmail.com>
2026-09-01 00:45:23 +0800

    Add files via upload

--- tray.js
+++ tray.js
@@ -1,6 +1,7 @@
-document.getElementById('tm-open').addEventListener('click',()=>window.native.trayAction('open'));
-document.getElementById('tm-hide').addEventListener('click',()=>window.native.trayAction('hide'));
-document.getElementById('tm-settings').addEventListener('click',()=>window.native.trayAction('settings'));
-document.getElementById('tm-reload').addEventListener('click',()=>window.native.trayAction('menu-reload'));
-document.getElementById('tm-quit').addEventListener('click',()=>window.native.trayAction('quit'));
-window.addEventListener('keydown',e=>{if(e.key==='Escape')window.close()});
+const send = action => window.native.trayAction(action);
+document.getElementById('tm-open')?.addEventListener('click', () => send('open'));
+document.getElementById('tm-hide')?.addEventListener('click', () => send('hide'));
+document.getElementById('tm-reload')?.addEventListener('click', () => send('menu-reload'));
+document.getElementById('tm-site')?.addEventListener('click', () => send('visit-site'));
+document.getElementById('tm-quit')?.addEventListener('click', () => send('quit'));
+window.addEventListener('keydown', e => { if (e.key === 'Escape') window.close(); });
